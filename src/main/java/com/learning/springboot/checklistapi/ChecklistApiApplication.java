53877e982b1dabb8a5066074befc6587a6385049
Carlos Lazarin <carlos.lazarin@gmail.com>
2021-05-25 08:57:28 -0300

    Adding cors policy for AWS

--- src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
+++ src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
@@ -40,7 +40,7 @@ public class ChecklistApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200")
+						.allowedOrigins("http://checklist-spa.s3-website-sa-east-1.amazonaws.com")
 						.allowedMethods("GET", "PUT", "OPTIONS", "POST", "DELETE", "PATCH")
 						.maxAge(900)
 						.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization");
